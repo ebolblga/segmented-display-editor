6d99d15383ba4a9d33aa582771bc68eb9c2e8e51
ebolblga <kkochanovskiy@gmail.com>
2025-08-31 22:06:20 +0300

    fix: add sitemap and google-site-verification tag

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+useHead({
+    meta: [
+        // Google site verification
+        {
+            name: 'google-site-verification',
+            content: 'i853Frcg3ekn2nNVhPVPrR6gzydjSnp_Izo6HorOSBY',
+        },
+    ],
+})
+</script>
 <template>
     <NuxtLayout>
         <NuxtPage />
